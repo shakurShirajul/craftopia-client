592233928e4ba30ca0e0b03ba0b59a820d8f18d0
shakurShirajul <siirajulisllam@gmail.com>
2024-04-25 22:20:44 +0600

    Project Initialized

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>
+    },
+]);
+
+export default router;
