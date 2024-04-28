f45cdf52847e8edcfb053dfdcb8a7006c7a69a54
shakurShirajul <siirajulisllam@gmail.com>
2024-04-28 23:37:10 +0600

    Loading Spiner Added In View Details Page

--- src/pages/ItemDetails/ItemDetails.jsx
+++ src/pages/ItemDetails/ItemDetails.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const ItemDetails = () => {
     const [craftItem, setCraftItem] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const { id } = useParams();
 
@@ -10,29 +11,43 @@ const ItemDetails = () => {
         fetch(`https://craftopia-server-ruddy.vercel.app/item/${id}`)
             .then(res => res.json())
             .then(data => setCraftItem(data))
+        setLoading(false)
     }, [])
     console.log(craftItem);
     return (
-        <div className='md:w-3/5 md:mx-auto my-10 bg-white'>
-            <div className='grid grid-cols-2 gap-5 p-5'>
-                <div>
-                    <img src={craftItem.image} alt="" className='aspect-square' />
-                </div>
-                <div className='font-inter'>
-                    <p className='text-4xl'>{craftItem.item_name}</p>
-                    <div className='flex items-center gap-2'>
-                        <p>{craftItem.subcategory_name}</p>
-                        <span> | </span>
-                        <p>{craftItem.rating}</p>
-                    </div>
+        <div>
+            {
+                loading ?
+                    (
+                        <div className="text-center my-10">
+                            <span className="loading loading-infinity loading-lg"></span>
+                        </div>
+                    )
+                    :
+                    (
+                        <div className='md:w-3/5 md:mx-auto my-10 bg-white'>
+                            <div className='grid grid-cols-2 gap-5 p-5'>
+                                <div>
+                                    <img src={craftItem.image} alt="" className='aspect-square' />
+                                </div>
+                                <div className='font-inter'>
+                                    <p className='text-4xl'>{craftItem.item_name}</p>
+                                    <div className='flex items-center gap-2'>
+                                        <p>{craftItem.subcategory_name}</p>
+                                        <span> | </span>
+                                        <p>{craftItem.rating}</p>
+                                    </div>
 
-                    <p className='text-lg'>{craftItem.short_description}</p>
-                    <p>$ {craftItem.price}</p>
+                                    <p className='text-lg'>{craftItem.short_description}</p>
+                                    <p>$ {craftItem.price}</p>
 
-                    <p>Customization: {craftItem.customization}</p>
-                    <p>Procising Time: {craftItem.processing_time}</p>
-                </div>
-            </div>
+                                    <p>Customization: {craftItem.customization}</p>
+                                    <p>Procising Time: {craftItem.processing_time}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+            }
         </div>
     );
 };
